1bd849cec98cb910b2ab24f64c72312c9f20b07c
damiengallagher <damien.gallagher@gmail.com>
2020-10-21 13:51:53 +0100

    Added styles to page

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import axios from 'axios';
+import styles from './App.module.css';
+
+import styled from 'styled-components';
+
+const StyledContainer = styled.div` height: 100vw;
+padding: 20px;
+background: #83a4d4;
+background: linear-gradient(to left, #b6fbff, #83a4d4);
+color: #171212; `;
+const StyledHeadlinePrimary = styled.h1` font-size: 48px;
+font-weight: 300;
+letter-spacing: 2px;
+`;
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -44,30 +57,31 @@ const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
-  const [url, setUrl] = React.useState( `${API_ENDPOINT}${searchTerm}`);
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
 
   const handleSearchInput = event => { setSearchTerm(event.target.value); };
 
-  const handleSearchSubmit = () => { setUrl(`${API_ENDPOINT}${searchTerm}`);};
+  const handleSearchSubmit = event => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+    event.preventDefault();
+  };
 
   const [stories, dispatchStories] = React.useReducer(storiesReducer,
     { data: [], isLoading: false, isError: false }
   );
 
-  const handleFetchStories = React.useCallback(() => { // B
+  const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    console.log("here 1 url:"+url)
-    axios.get(url)
-      .then(result => {
-        console.log("result")
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS', 
-          payload: result.data.hits,
-        });
-      }).catch(() =>
-        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-      );
-  }, [url]); // E
+    try {
+      const result = await axios.get(url);
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS', payload: result.data.hits,
+      });
+
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
+  }, [url]);
 
 
   React.useEffect(() => {
@@ -81,23 +95,13 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
-
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearchInput}>
-        <strong>Search:</strong>
-      </InputWithLabel>
-
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}>
-        Submit
-</button>
+    <StyledContainer>
+    <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
 
       <hr />
       {stories.isError && <p>Something went wrong ...</p>}
@@ -107,7 +111,7 @@ const App = () => {
             list={stories.data}
             onRemoveItem={handleRemoveStory}
           />)}
-    </div>
+    </StyledContainer>
   );
 };
 
@@ -122,13 +126,16 @@ const InputWithLabel = ({
   children,
 }) => (
     <>
-      <label htmlFor={id}>{children} </label>
+      <label htmlFor={id} className={styles.label}>
+        {children}
+      </label>
       <input
         id={id}
         type={type}
         autoFocus={isFocused}
         onChange={onInputChange}
-        value={value} />
+        value={value}
+        className={styles.input} />
     </>
   );
 
@@ -141,20 +148,41 @@ const List = ({ list, onRemoveItem }) =>
   />);
 
 const Item = ({ item, onRemoveItem }) => (
-  <div>
-    <span>
+  <div className={styles.item}>
+    <span style={{ width: '40%' }}>
       <a href={item.url}>{item.title}</a>
     </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={() => onRemoveItem(item)}>
-        Dismiss
-      </button>
+    <span style={{ width: '30%' }}>{item.author}</span>
+    <span style={{ width: '10%' }}>{item.num_comments}</span>
+    <span style={{ width: '10%' }}>{item.points}</span>
+    <span style={{ width: '10%' }}>
+      <button
+        type="button"
+        onClick={() => onRemoveItem(item)} className={`${styles.button} ${styles.buttonSmall}`}
+      > Dismiss
+          </button>
     </span>
-  </div>
-);
+  </div>);
 
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}) => (
+    <form onSubmit={onSearchSubmit} className={styles.searchForm}>
+      <InputWithLabel
+        id="search" value={searchTerm}
+        isFocused onInputChange={onSearchInput}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
+      <button
+        type="submit"
+        disabled={!searchTerm}
+        className={`${styles.button} ${styles.buttonLarge}`}>
+        Submit
+      </button>
+    </form>
+  );
 
 export default App;

42ed54274a51b0022161b7c3dc754cb942de1ee9
Damien Gallagher <damien.gallagher@gmail.com>
2020-11-16 23:07:37 +0000

    Initial commit of the source

--- react-udemy/react-complete-guide/src/App.js
+++ react-udemy/react-complete-guide/src/App.js
@@ -1,21 +1,92 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+class App extends Component{
+
+  state = {
+    persons: [
+      { id:1, name: 'Max', age: 28},
+      { id:2, name: 'Peter', age: 22},
+      { id:3, name: 'Paul', age: 36}
+    ],
+    otherState: 'Some other state',
+    showPersons: false
+  } 
+
+  nameChangedHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons : !doesShow});
+  }
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
+  render(){
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+    
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+                    key={person.id}
+                    name={person.name} 
+                    age={person.age}
+                    click={() => this.deletePersonHandler(index)}
+                    changed={(event) => this.nameChangedHandler(event, person.id)}/>
+          })}
+          </div>
+      );
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Hi, I am a React App</h1>
+      <button style={style}
+        onClick={this.togglePersonsHandler}>
+        Toggle Persons
+      </button>
+
+        {persons}
+
       </div>
+      // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I am a React App!!!'));
     );
   }
+   
 }
+
 
 export default App;

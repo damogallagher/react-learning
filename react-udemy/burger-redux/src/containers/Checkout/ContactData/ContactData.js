0c0788638f6803f268274664abc468c0953a7603
Damien Gallagher <damien.gallagher@gmail.com>
2020-12-10 08:31:47 +0000

    Changed project to use redux

--- react-udemy/burger-redux/src/containers/Checkout/ContactData/ContactData.js
+++ react-udemy/burger-redux/src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,8 @@ import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
+import { connect } from 'react-redux';
+
 class ContactData extends Component {
     state = {
         orderForm: {
@@ -93,7 +95,7 @@ class ContactData extends Component {
 
     orderHandler = (event) => {
         event.preventDefault();
-        console.log(this.props.ingredients);
+        console.log(this.props.ings);
 
         this.setState( { loading: true } );
         const formData = {}
@@ -101,7 +103,7 @@ class ContactData extends Component {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
         }
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData            
         }
@@ -191,4 +193,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    };
+}
+
+export default connect(mapStateToProps)(ContactData);

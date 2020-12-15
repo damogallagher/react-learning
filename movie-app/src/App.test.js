5583ae20ae3da04f9c056ac194a8ec73c054fcbf
damiengallagher <damien.gallagher@gmail.com>
2020-12-15 22:09:03 +0000

    Update before adding tests

--- movie-app/src/App.test.js
+++ movie-app/src/App.test.js
@@ -1,8 +1,17 @@
 import React, {render, screen} from '@testing-library/react';
 import App from './App';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import AppContainer from './containers/AppContainer';
+configure({adapter: new Adapter()});
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('should render <AppContainer /> ', () => {
+    expect(wrapper.find(AppContainer)).toHaveLength(1);
+  });
 });
